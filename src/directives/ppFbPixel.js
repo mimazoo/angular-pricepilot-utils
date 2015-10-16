55496eeaa30bb3d4fd04e30930203cebcbf2f0c7
Tomislav Slijepcevic <tomi.slijepcevic@gmail.com>
2015-10-16 14:59:04 +0200

    Fixed error 'Duplicate pixel id'

--- src/directives/ppFbPixel.js
+++ src/directives/ppFbPixel.js
@@ -9,7 +9,6 @@ angular.module('ngPricepilotUtils')
             n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
             t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
                 document,'script','//connect.facebook.net/en_US/fbevents.js');
-
         }
 
         return {
@@ -23,8 +22,7 @@ angular.module('ngPricepilotUtils')
         };
 
         function link(scope) {
-
-            if (!window.__fbq) {
+            if (!window._fbq) {
                 insertFbScript();
                 fbq('init', {});
             }
