bca8de66b11960e810f88f236fe5abd4e25d3f23
Tomislav Slijepcevic <tomi.slijepcevic@gmail.com>
2015-12-04 13:08:37 +0100

    Fixed FB pixel directive.

--- src/directives/ppFbPixel.js
+++ src/directives/ppFbPixel.js
@@ -5,34 +5,35 @@ angular.module('ngPricepilotUtils')
 
     function insertFbScript() {
         !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-                document,'script','//connect.facebook.net/en_US/fbevents.js');
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+        n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+        document,'script','//connect.facebook.net/en_US/fbevents.js');
+    }
+
+    return {
+        link: link,
+        restrict: 'EA',
+        scope: {
+            pixelID: '@',
+            ev: '@',
+            value: '@',
+            currency: '@'
+        }
+    };
+
+    function link(scope) {
+        if (!window._fbq) {
+            insertFbScript();
+            fbq('init', scope.pixelID);
         }
 
-        return {
-            link: link,
-            restrict: 'EA',
-            scope: {
-                ev: '@',
-                value: '@',
-                currency: '@'
-            }
-        };
+        var ev = scope.ev;
+        var value = scope.value || '0.00';
+        var currency = scope.currency || 'USD';
 
-        function link(scope) {
-            if (!window._fbq) {
-                insertFbScript();
-                fbq('init', {});
-            }
+        fbq('track', ev, {value: value, currency: currency});
 
-            var ev = scope.ev;
-            var value = scope.value || '0.00';
-            var currency = scope.currency || 'USD';
-
-            fbq('track', ev, {value: value, currency: currency});
-
-            console.log(["FB conversion", ev, value, currency].join(' '));
-        }
-    });
+        console.log(["FB conversion", ev, value, currency].join(' '));
+    }
+});
